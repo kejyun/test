7ddc6c7a431f73e9c38bb58cf88063ee36ebee57
KeJyun <kejyun@gmail.com>
2016-10-19 17:42:46 +0800

    no message

--- js/player.js
+++ js/player.js
@@ -7,3 +7,17 @@ videojs.plugin('listenForPlayButton', function() {
   };
  window.addEventListener("message",playVideo);
 });
+
+videojs.ready(function() {
+  var myPlayer = this,
+    bclsLog = document.getElementById('bclsLog');
+    myPlayer.on('fullscreenchange', function(evt) {
+    if (myPlayer.isFullscreen()) {
+      // log the event
+      bclsLog.innerHTML += '<p class="bcls-red">Fullscreen on at ' + myPlayer.currentTime() + ' seconds</p>';
+    } else {
+      // log the event
+      bclsLog.innerHTML += '<p class="bcls-green">Fullscreen off at ' + myPlayer.currentTime() + ' seconds</p>';
+    }
+  });
+});
