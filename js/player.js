cf9248d1a593f4febeeb367b2b67f216262d5b33
KeJyun <kejyun@gmail.com>
2016-10-20 11:30:44 +0800

    no message

--- js/player.js
+++ js/player.js
@@ -1,21 +1,21 @@
-videojs.plugin('listenForPlayButton', function() {
-  var player = this,
-     playVideo = function(evt){
-      if(evt.data === "playVideo"){
-        player.play();
-     }
-  };
- window.addEventListener("message",playVideo);
-});
+// videojs.plugin('listenForPlayButton', function() {
+//   var player = this,
+//      playVideo = function(evt){
+//       if(evt.data === "playVideo"){
+//         player.play();
+//      }
+//   };
+//  window.addEventListener("message",playVideo);
+// });
 
-videojs.plugin('listenForPlay', function(){
-    var player = this;
-    player.on('play', function(){
-        console.log(parent.location, "play");
-        parent.postMessage("videoPlayEvent", "*");
-    });
-    player.on('pause', function(){
-        console.log(parent.location, "pause");
-        parent.postMessage("videoPauseEvent", "*");
-    });
-});
+// videojs.plugin('listenForPlay', function(){
+//     var player = this;
+//     player.on('play', function(){
+//         console.log(parent.location, "play");
+//         parent.postMessage("videoPlayEvent", "*");
+//     });
+//     player.on('pause', function(){
+//         console.log(parent.location, "pause");
+//         parent.postMessage("videoPauseEvent", "*");
+//     });
+// });
