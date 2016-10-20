4d136918c0629280946ddf383248f9f0be64d388
KeJyun <kejyun@gmail.com>
2016-10-20 11:38:23 +0800

    no message

--- js/player.js
+++ js/player.js
@@ -7,6 +7,13 @@
 //   };
 //  window.addEventListener("message",playVideo);
 // });
+var consolere = {
+channel:'try-3c69-8e7f-5665',
+api:'//console.re/connector.js',
+ready: function(c) {var d=document,s=d.createElement('script'),l;s.src=this.api;s.id='consolerescript';s.onreadystatechange=s.onload=function(){if(!l){c();}l=true;};d.getElementsByTagName('head')[0].appendChild(s);}};
+
+consolere.ready({ console.re.log('remote log test'); });
+
 
 videojs.plugin('listenForPlay', function(){
     var player = this;
