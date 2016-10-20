523d7d3f52e3dbbaa3dd125db95ad76c882adb0f
KeJyun <kejyun@gmail.com>
2016-10-20 11:27:17 +0800

    no message

--- js/player.js
+++ js/player.js
@@ -8,16 +8,14 @@ videojs.plugin('listenForPlayButton', function() {
  window.addEventListener("message",playVideo);
 });
 
-videojs.ready(function() {
-  var myPlayer = this,
-    bclsLog = document.getElementById('bclsLog');
-    myPlayer.on('fullscreenchange', function(evt) {
-    if (myPlayer.isFullscreen()) {
-      // log the event
-      bclsLog.innerHTML += '<p class="bcls-red">Fullscreen on at ' + myPlayer.currentTime() + ' seconds</p>';
-    } else {
-      // log the event
-      bclsLog.innerHTML += '<p class="bcls-green">Fullscreen off at ' + myPlayer.currentTime() + ' seconds</p>';
-    }
-  });
+videojs.plugin('listenForPlay', function(){
+    var player = this;
+    player.on('play', function(){
+        console.log(parent.location, "play");
+        parent.postMessage("videoPlayEvent", "*");
+    });
+    player.on('pause', function(){
+        console.log(parent.location, "pause");
+        parent.postMessage("videoPauseEvent", "*");
+    });
 });
